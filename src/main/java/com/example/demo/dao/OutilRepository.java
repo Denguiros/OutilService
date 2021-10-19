93a2f6e2bc619b182fb361f8d14d02c4c77119fe
Denguiros <fehmi.denguir@enis.tn>
2021-10-20 00:23:53 +0100

    Added service and controller

--- src/main/java/com/example/demo/dao/OutilRepository.java
+++ src/main/java/com/example/demo/dao/OutilRepository.java
@@ -4,12 +4,14 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entities.Outil;
 
-@Repository
+@RepositoryRestController
 public interface OutilRepository extends JpaRepository<Outil, Long> {
 	List<Outil> findBySource(String source);
+	List<Outil> findAll();
 	Outil findByDate(Date date);
 }
