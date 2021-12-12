a3ce026407ee43f670886e5760bd87a4855c4645
Denguiros <fehmi.denguir@enis.tn>
2021-12-12 16:40:24 +0100

    Added link between microservices

--- src/main/java/com/example/demo/OutilService.java
+++ src/main/java/com/example/demo/OutilService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 import com.example.demo.dao.OutilRepository;
 import com.example.demo.entities.Outil;
@@ -13,6 +14,8 @@ public class OutilService implements CommandLineRunner {
 
 	@Autowired
 	OutilRepository outilRepository;
+	@Autowired
+	RepositoryRestConfiguration configuration;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OutilService.class, args);
@@ -20,6 +23,7 @@ public class OutilService implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		configuration.exposeIdsFor(Outil.class);
 		Outil outil = new Outil();
 		outilRepository.save(outil);
 	}
